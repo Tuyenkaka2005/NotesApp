c49f19c043ffaa4a4d4f545ecbf27c21005763b4
Tuyenkaka2005 <nguyendangtuyen2005@gmail.com>
2026-05-15 00:58:52 -0700

    Initial Commit

---
+++ NotesApp/NotesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotesAppApp.swift
+//  NotesApp
+//
+//  Created by Tuyen on 15/5/26.
+//
+
+import SwiftUI
+
+@main
+struct NotesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
